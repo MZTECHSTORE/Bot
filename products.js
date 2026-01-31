26e002b26644a17892e96ea416a5cbb99c882a1e
MZTECHSTORE <daltonantonio450@gmail.com>
2026-01-31 03:48:16 +0200

    Create products.js

---
+++ products.js
@@ -0,0 +1,18 @@
+module.exports = {
+  netflix: {
+    "1": { name: "Netflix Premium", price: 200, delivery: "account" },
+    "2": { name: "Netflix Ultra HD", price: 400, delivery: "account" }
+  },
+  ebooks: {
+    "1": { name: "eBooks Básico", price: 300, delivery: "link" },
+    "2": { name: "eBooks Completo", price: 500, delivery: "link" }
+  },
+  logotipo: {
+    "1": { name: "Logotipo para Empresas", price: 1000, delivery: "manual" },
+    "2": { name: "Logotipo para Panfletos / Músicas", price: 500, delivery: "manual" },
+    "3": { name: "Logotipo para Camisetas", price: 400, delivery: "manual" }
+  },
+  curriculo: {
+    "1": { name: "Currículo Profissional", price: 1000, delivery: "manual" }
+  }
+};
